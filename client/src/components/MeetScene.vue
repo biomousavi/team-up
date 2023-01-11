41943fa6ca4070f252694efb75c8ae5add3cb837
mohammad hosein mousavi <biomousavi@gmail.com>
2023-01-11 22:48:14 +0330

    fix: dynamic layout fraction

--- client/src/components/MeetScene.vue
+++ client/src/components/MeetScene.vue
@@ -4,9 +4,11 @@ import { useMeetStore } from '@/stores/meet';
 import VideoStream from './VideoStream.vue';
 import { useUserStore } from '@/stores/user';
 import MeetNavigation from '@/components/MeetNavigation.vue';
+import { useDisplay } from 'vuetify/lib/framework.mjs';
 
 const meet = useMeetStore();
 const user = useUserStore();
+const display = useDisplay();
 
 onUnmounted(meet.leaveRoom);
 
@@ -19,24 +21,49 @@ const sceneFraction = computed(() => {
   }
 
   let fraction: number = 1;
+  // only we had more than one user
   if (meetingUsers > 1) fraction = Math.ceil(meetingUsers / root);
 
   return `repeat(${fraction}, 1fr)`;
 });
+
+const sceneLayout = computed(() => {
+  let gridTemplate: any = { 'grid-template-columns': sceneFraction.value };
+
+  // if we had only two users, apply row direction
+  if (display.smAndDown.value && meet.users.length === 2) {
+    gridTemplate = { 'grid-template-rows': sceneFraction.value };
+  }
+
+  return gridTemplate;
+});
 </script>
 
 <template>
-  <div class="scene">
+  <XyzTransitionGroup
+    :style="sceneLayout"
+    appear
+    class="scene"
+    xyz="fade small out-down out-rotate-right appear-stagger"
+  >
     <!-- local stream -->
-    <VideoStream v-if="meet.localStream" :data="{name:user.name!, stream:meet.localStream!}" />
+    <VideoStream
+      key="local-stream"
+      v-if="meet.localStream"
+      :data="{name:user.name!, stream:meet.localStream!}"
+    />
 
     <!-- remote streams -->
     <template v-for="user of meet.users" :key="user.id">
-      <VideoStream v-if="user.mediaStream" :data="{name:user.name!, stream: user.mediaStream}" />
+      <VideoStream
+        :key="user.id"
+        v-if="user.mediaStream"
+        :data="{name:user.name!, stream: user.mediaStream}"
+      />
     </template>
 
-    <MeetNavigation />
-  </div>
+    <MeetNavigation key="chat" />
+  </XyzTransitionGroup>
 </template>
 
 <style scoped>
@@ -45,7 +72,6 @@ const sceneFraction = computed(() => {
   width: 100%;
   display: grid;
   gap: 5px;
-  grid-template-columns: v-bind(sceneFraction);
   transition-duration: 0.6s;
   transition-delay: 0.1s;
   transition-property: all;
