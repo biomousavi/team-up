8993adff321b6d2985dd9611b915e1995af06ee7
mohammad hosein mousavi <biomousavi@gmail.com>
2023-01-12 11:03:51 +0330

    chore: remove comments

--- server/src/main.ts
+++ server/src/main.ts
@@ -16,23 +16,11 @@ async function bootstrap() {
     }),
   );
 
-  // await app.register(fastifyExpress);
-
-  // const signalingPath = '/signal';
-
-  // app.use(signalingPath, (req, res) => {
-  //   console.log('asdas');
-  //   peerServer(req, res);
-  // });
-  // app.getHttpAdapter().use(signalingPath, peerServer);
-
   const configService: ConfigService = app.get(ConfigService);
 
   await app.register(fastifyCors, { credentials: true, origin: '*' });
   await app.register(fastifyHelmet, { contentSecurityPolicy: false });
-  const server = await app.listen(configService.get<number>('SERVER_PORT'), '0.0.0.0');
-
-  // const peerServer = PeerServer({ path: signalingPath });
+  await app.listen(configService.get<number>('SERVER_PORT'), '0.0.0.0');
 
   console.log(`server is running on: ${await app.getUrl()}`);
 }
