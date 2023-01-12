46e8d083cc079f9bd0f7d2d51e1f25107e4e7e5e
mohammad hosein mousavi <biomousavi@gmail.com>
2023-01-12 17:37:19 +0330

    fix: fix client connection

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -7,6 +7,8 @@ import type { ReservedEvent, MeetEvent, User, SignalPayload } from './types';
 const socket = io(import.meta.env.VITE_SERVER_URL, {
   autoConnect: false,
   reconnection: true,
+  transports: ['websocket', 'polling'],
+  secure: true,
   path: '/',
 });
 
