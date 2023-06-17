4f94424437233e3edea8fa176cbc3f302788660a
Mohammad hosein Mousavi <hmohammad@prosper.az>
2023-06-17 16:22:09 +0330

    docs: add comment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,23 +1,32 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
 export default defineConfig(() => {
   return {
-    plugins: [vue(), vuetify({ styles: { configFile: 'src/assets/settings.scss' } })],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
-    },
+    plugins: [
+      // vue plugin to work withg vue framework
+      vue(),
+
+      // to auto import vuetify components and configurations
+      vuetify({ styles: { configFile: 'src/assets/settings.scss' } }),
+    ],
+
+    // add path alias
+    resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    define: { global: {} },
+
+    // define env file directory
     envDir: '../',
+
+    // config vite dev server
     server: {
       host: '0.0.0.0',
-      hmr: {
-        host: 'localhost',
-      },
+      hmr: { host: 'localhost' },
       port: parseInt(process.env.VITE_SERVER_PORT!) || 3000,
     },
   };
